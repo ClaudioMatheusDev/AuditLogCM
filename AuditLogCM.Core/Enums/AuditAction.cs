0acdc77f1317724bc52da4a7322565590c1e40ba
Claudio Matheus <claudiomatheus055@gmail.com>
2026-06-01 21:24:47 -0300

    feat: adicionando a estrutura inicial do projeto com o núcleo, o EFCore e os testes;

---
+++ AuditLogCM.Core/Enums/AuditAction.cs
@@ -0,0 +1,9 @@
+﻿namespace AuditLogCM.Core.Enums
+{
+    public enum AuditAction
+    {
+        Create = 1,
+        Update = 2,
+        Delete = 3
+    }
+}
---
+++ AuditLogCM.Core/Models/AuditEntry.cs
@@ -0,0 +1,18 @@
+﻿
+namespace AuditLogCM.Core.Models
+{
+    public class AuditEntry
+    {
+        public Guid IDAuditEntry { get; set; }
+        public string IDTabelaAfetada { get; set; } = string.Empty;
+        public string NomeTabelaAfetada { get; set; } = string.Empty;
+        public string? ValoresAnteriores { get; set; }
+        public string? ValoresNovos { get; set; }
+        public Enums.AuditAction Acao { get; set; }
+        public string? IDUsuario { get; set; }
+        public string NomeUsuario { get; set; } = string.Empty;
+        public DateTime DataAlteracao { get; set; } = DateTime.UtcNow;
+
+
+    }
+}
